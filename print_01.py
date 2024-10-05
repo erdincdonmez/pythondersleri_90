a415d4a6b048667185760e8c0d1977748917a971
erdincDonmez <erdinc_donmez@yahoo.com>
2024-10-05 13:03:07 +0300

    ilk ders

---
+++ print_01.py
@@ -0,0 +1,11 @@
+print("Merhaba")
+# print("vektorel "*5)
+abc = 8*3 # abc bir değişkendir.
+# Değişkenler tabaktır.
+# abc = 10
+abc = 5
+10+2
+abc = "vektorel"
+print(abc)
+print(8*3)
+print("8*3")
